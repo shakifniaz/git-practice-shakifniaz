dd7e5175d102c661350c64809bb93b4918515669
shakifniaz <shakifniaz@gmail.com>
2026-06-14 00:49:45 +0600

    Add Multiplication and Division function to utils and main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from utils import add, subtract
+from utils import add, subtract, multiply, divide
 
 print("Name: Your Name")
 print("Date:", datetime.today().strftime("%d-%m-%Y"))
 
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+print("Multiplication:", multiply(5, 3))
+print("Division:", divide(15, 3))
